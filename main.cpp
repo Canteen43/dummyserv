b3a0f8efcdb32488bb5a3d6a8c09563e3581f176
Canteen43 <karl.weihmann@gmail.com>
2025-04-28 17:01:32 +0200

    I am commited

--- main.cpp
+++ main.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 #include <sys/socket.h>	// socket()
 #include <arpa/inet.h>	// htons()
+#include <netinet/in.h>	// sockaddr_in
 
 // #include <netinet/in.h>	// sockaddr_in
 int main()
 {
 	std::cout << "This is a cool test\n";
+
 	int socketFD = socket(AF_INET, SOCK_STREAM, 0);
-	uint16_t testInt = htons(257);
 	std::cout << "Socket value: " << socketFD << "\n";
-	std::cout << "Testint value: " << testInt << "\n";
+
+	sockaddr_in address;
+	address.sin_port = htons(8080);
+	address.sin_family = AF_INET;
+	address.sin_addr = INADDR_ANY;
+
+	
 }
